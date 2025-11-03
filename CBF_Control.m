ed7ba49d8520fd291da60d603374ba7080c0f1e8
GuidoCazz <72558065+GuidoCazz@users.noreply.github.com>
2025-11-03 12:16:11 -0500

    Update ROS configuration and trajectory handling
    Updated ROS IP and MASTER URL for new network configuration. Added handling for matfile updates and adjusted trajectory calculations.

--- CBF_Control.m
+++ CBF_Control.m
@@ -10,11 +10,11 @@ addpath(genpath(qcm_path));
 addpath(genpath(ad_path));
 
 rng default;
-rosIP   = '10.229.22.25';                 
-MASTER  = 'http://10.229.22.87:11311';
+rosIP   = '129.97.71.119';                 
+MASTER  = 'http://129.97.71.85:11311';
 CONTROL_OUT = 'cmd_vel';
 
-
+L = 0.2;
 LAMBDA = 5e10;
 DT = 0.001;
 T_MAX = 1e4;
@@ -36,16 +36,51 @@ pub = rospublisher(CONTROL_OUT,'geometry_msgs/Twist');
 % Initialize the ROS node and set up the mapping process
 disp('ROS Control:');
 
-    
+disp('Matfile arrived')
+% Se il file è stato modificato, aggiorna le mappature
+m = matfile(matfile_path);
+WM = m.wm;
+realWorld = m.realWorld;
+ballWorld = m.ballWorld;
+
+WM.evaluateMappings(LAMBDA);
+[r2bMap, b2rMap, r2bJac, b2rJac] = WM.getMappings();
+        
+Nobst = numel(realWorld.obstacles);
+
 xTrajBall = zeros(2,T_MAX);
 xTraj = zeros(2,T_MAX);
 
 count = 0;
 for t = 1 : T_MAX
     % Controlla se il matfile è stato modificato
+
+    try
+        tfBase = getTransform(tftree, 'map', 'base_link');            
+        % Extract position
+        x = [tfBase.Transform.Translation.X; tfBase.Transform.Translation.Y];
+
+        q = [tfBase.Transform.Rotation.W, tfBase.Transform.Rotation.X, tfBase.Transform.Rotation.Y, tfBase.Transform.Rotation.Z];
+        eul = quat2eul(q);       % default ZYX
+        theta = eul(1);
+        x,theta
+    catch
+        warning('TF not available at this timestep');
+    end
+
+    xBall = r2bMap(x);
+    % xG = [4.5;-4.85];
+    xG = [3.5;0];
+    xGBall = r2bMap(xG);
+    
+
+    xTraj(:,t) = x;
+    xTrajBall(:,t) = xBall;
+    
     file_info = dir(matfile_path);
     if file_info.datenum > last_modified_time==0 || t==1
 
+        disp('Matfile arrived')
         % Se il file è stato modificato, aggiorna le mappature
         m = matfile(matfile_path);
         WM = m.wm;
@@ -58,23 +93,11 @@ for t = 1 : T_MAX
         last_modified_time = file_info.datenum; % Aggiorna la data di modifica
         Nobst = numel(realWorld.obstacles);
         
-
-        try
-            tfBase = getTransform(tftree, 'map', 'base_link');            
-            % Extract position
-            x = [tfBase.Transform.Translation.X; tfBase.Transform.Translation.Y];
-
-            q = [tfBase.Transform.Rotation.W, tfBase.Transform.Rotation.X, tfBase.Transform.Rotation.Y, tfBase.Transform.Rotation.Z];
-            eul = quat2eul(q);       % default ZYX
-            theta = eul(1);
-        catch
-            warning('TF not available at this timestep');
-        end
         
 
         xBall = r2bMap(x);
         % xG = [4.5;-4.85];
-        xG = [3;-2];
+        xG = [3.5;0];
         xGBall = r2bMap(xG);
         
 
@@ -82,6 +105,7 @@ for t = 1 : T_MAX
         xTrajBall(:,t) = xBall;
 
         % Plot iniziale
+        close all;
         figure;
         subplot(1,2,1), hold on, axis equal, set(gca, 'Visible', 'off')
         hSTraj = line(x(1), x(2), 'LineWidth', 2, LineJoin="chamfer");
@@ -92,7 +116,7 @@ for t = 1 : T_MAX
         scatter(xG(1), xG(2), 200, '.');
         hRobot = drawRobot(x(1), x(2), theta, 0.2, 'r');
 
-        
+
         subplot(1,2,2), hold on, axis equal, set(gca, 'Visible', 'off')
         hSBall = scatter(xBall(1), xBall(2), 200, '.');
         hSTrajBall = line(xBall(1), xBall(2), 'LineWidth', 2, LineJoin="chamfer");
@@ -104,7 +128,7 @@ for t = 1 : T_MAX
             hObstBall{i} = plot(obstBall(1,[1:end,1]), obstBall(2,[1:end,1]), 'LineWidth', 2, LineJoin="chamfer");
         end
         hGBall = scatter(xGBall(1), xGBall(2), 200, '.');
-        
+
         drawnow
     end
         
@@ -133,8 +157,8 @@ for t = 1 : T_MAX
     bcbf = zeros(Nobst,1);
     for i = 1 : Nobst
         Acbf(i,2*i-1:2*i) = 2*(xBall-ballWorld.obstacles{i}.center)';
-        Acbf(i,2*Nobst+i) = 2*ballWorld.obstacles{i}.radius;
-        bcbf(i) = 2*(xBall-ballWorld.obstacles{i}.center)'*uBall + 0.5e2 * (norm(xBall-ballWorld.obstacles{i}.center)^2-ballWorld.obstacles{i}.radius^2);
+        Acbf(i,2*Nobst+i) = 2*(ballWorld.obstacles{i}.radius + L);
+        bcbf(i) = 2*(xBall-ballWorld.obstacles{i}.center)'*uBall + 100 * (norm(xBall-ballWorld.obstacles{i}.center)^2-(ballWorld.obstacles{i}.radius + L)^2);
     end
     if strcmp(POSITION_RADIUS, 'move')
         Wcenter = 1;
@@ -173,29 +197,36 @@ for t = 1 : T_MAX
         x = b2rMap(xBall, x);
     else
         Jr2b = AutoDiffJacobianFiniteDiff(r2bMap, x);%timeAD=toc;
-        R = [cos(theta) -sin(theta);
-             sin(theta)  cos(theta)];
+        % R = [cos(theta) -sin(theta);
+        %      sin(theta)  cos(theta)];
+        d = [cos(theta); sin(theta)]; 
 
 %       u = r2bJac(x) \ uBall;
+
         u = Jr2b \ uBall;
 
-        v = R' * u;
-        v_max = 1.0;
-        if abs(v(1)) > v_max
-            v(1) = sign(v(1)) * v_max;
-        end
+        v_proj = dot(d, u) / (dot(d,d))/500;   % scala (può essere negativa)
+        v_max = inf;
+        v = max(min(v_proj, v_max), -v_max);  % clamp preservando segno
+        % if abs(uBall(1)) > v_max || abs(uBall(2)) > v_max
+        %     uBall(1) = sign(uBall(1)) * v_max;
+        %     uBall(2) = sign(uBall(2)) * v_max;
+        % end
 
-        disp(v);
-        d = [cos(theta); sin(theta)]; 
         dotp = dot(d,u);              
         crossp = d(1)*u(2) - d(2)*u(1);
         
         alpha = atan2(crossp, dotp);
+        omega = 1.5*alpha;
 
-        omega = 10*alpha;
+        % if abs(v) < 0.05
+        %     omega = 0.1*alpha;
+        % else
+        %     omega = 2*alpha;
+        % end
+        % x = x + v(1)*[cos(theta);sin(theta)]*DT;
+        % theta = theta + omega*DT;
 
-        x = x + v(1)*[cos(theta);sin(theta)]*DT;
-        theta = theta + omega*DT;
     end
 
     %%% plot
@@ -208,8 +239,8 @@ for t = 1 : T_MAX
     xTraj(:,t+1) = x;
     xTrajBall(:,t+1) = xBall;
     
-    % hS.XData = x(1);
-    % hS.YData = x(2);
+    hS.XData = x(1);
+    hS.YData = x(2);
     hSTraj.XData = xTraj(1,1:t+1);
     hSTraj.YData = xTraj(2,1:t+1);
     hSBall.XData = xBall(1);
@@ -224,9 +255,10 @@ for t = 1 : T_MAX
 
     cmd = rosmessage(pub);
 
-    cmd.Linear.X = v(1);
+    cmd.Linear.X = v;
     cmd.Angular.Z = omega;
 
     send(pub, cmd);
 end
+
 
